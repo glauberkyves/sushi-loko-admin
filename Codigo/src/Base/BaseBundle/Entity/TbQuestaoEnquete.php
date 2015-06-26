da883edbfe70544097fbe23abb26d1220df2be6e
Glauber Oliveira <gkyves@gmail.com>
2015-06-25 23:16:06 -0300

    init push

---
+++ Codigo/src/Base/BaseBundle/Entity/TbQuestaoEnquete.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Base\BaseBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * TbQuestaoEnquete
+ *
+ * @ORM\Table(name="tb_questao_enquete")
+ * @ORM\Entity
+ */
+class TbQuestaoEnquete
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id_questao_enquete", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idQuestaoEnquete;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="no_questao_enquete", type="text", length=65535, nullable=false)
+     */
+    private $noQuestaoEnquete;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="qt_votos", type="string", length=45, nullable=false)
+     */
+    private $qtVotos;
+
+
+}
+
