e3040556c5307ccc7aabfa1758195fa5bee94ff2
Fellipe Vasconcelos <fellipe.fvs@gmail.com>
2015-07-03 18:04:33 -0300

    correcoes em entidade franqueador e configuracao franquia

--- Codigo/src/Base/BaseBundle/Entity/TbFranqueador.php
+++ Codigo/src/Base/BaseBundle/Entity/TbFranqueador.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * TbFranqueador
  *
- * @ORM\Table(name="tb_franqueador")
+ * @ORM\Table(name="tb_franqueador", indexes={@ORM\Index(name="fk_franqueador_usuario_idx", columns={"id_usuario"}), @ORM\Index(name="fk_franqueador_endereco_idx", columns={"id_endereco"}), @ORM\Index(name="fk_franqueador_configuracaofranquia_idx", columns={"id_configuracao_franquia"})})
  * @ORM\Entity
  */
 class TbFranqueador extends AbstractEntity
@@ -22,46 +22,11 @@ class TbFranqueador extends AbstractEntity
     private $idFranqueador;
 
     /**
-     * @var TbPessoaJuridica
-     *
-     * @ORM\OneToOne(targetEntity="Base\BaseBundle\Entity\TbPessoa", mappedBy="idPessoa")
-     */
-    protected $idPessoa;
-
-    /**
-     * @var TbPessoaJuridica
-     *
-     * @ORM\OneToOne(targetEntity="Base\BaseBundle\Entity\TbEndereco", mappedBy="idPessoa")
-     */
-    private $idEndereco;
-
-    /**
      * @var integer
      *
      * @ORM\Column(name="st_niveis", type="integer", nullable=false)
      */
     private $stNiveis;
-
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="id_configuracao_franquia", type="integer", nullable=false)
-     */
-    private $idConfiguracaoFranquia;
-
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="no_responsavel", type="string", nullable=false)
-     */
-    private $noResponsavel;
-
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="no_email", type="string", nullable=false)
-     */
-    private $noEmail;
 
     /**
      * @var integer
@@ -76,6 +41,50 @@ class TbFranqueador extends AbstractEntity
      * @ORM\Column(name="dt_cadastro", type="datetime", nullable=false)
      */
     private $dtCadastro;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="no_responsavel", type="string", length=255, nullable=false)
+     */
+    private $noResponsavel;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="no_email", type="string", length=255, nullable=false)
+     */
+    private $noEmail;
+
+    /**
+     * @var \TbConfiguracaoFranquia
+     *
+     * @ORM\ManyToOne(targetEntity="Base\BaseBundle\Entity\TbConfiguracaoFranquia")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_configuracao_franquia", referencedColumnName="idtb_configuracao_franquia")
+     * })
+     */
+    private $idConfiguracaoFranquia;
+
+    /**
+     * @var \TbEndereco
+     *
+     * @ORM\ManyToOne(targetEntity="Base\BaseBundle\Entity\TbEndereco")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_endereco", referencedColumnName="id_endereco")
+     * })
+     */
+    private $idEndereco;
+
+    /**
+     * @var \TbUsuario
+     *
+     * @ORM\ManyToOne(targetEntity="Base\BaseBundle\Entity\TbUsuario")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_usuario", referencedColumnName="id_usuario")
+     * })
+     */
+    private $idUsuario;
 
     /**
      * @return int
@@ -94,38 +103,6 @@ class TbFranqueador extends AbstractEntity
     }
 
     /**
-     * @return TbPessoaJuridica
-     */
-    public function getIdPessoa()
-    {
-        return $this->idPessoa ? $this->idPessoa : new TbPessoa();
-    }
-
-    /**
-     * @param TbPessoaJuridica $idPessoa
-     */
-    public function setIdPessoa($idPessoa)
-    {
-        $this->idPessoa = $idPessoa;
-    }
-
-    /**
-     * @return int
-     */
-    public function getIdEndereco()
-    {
-        return $this->idEndereco ? $this->idEndereco : new TbEndereco();
-    }
-
-    /**
-     * @param int $idEndereco
-     */
-    public function setIdEndereco($idEndereco)
-    {
-        $this->idEndereco = $idEndereco;
-    }
-
-    /**
      * @return int
      */
     public function getStNiveis()
@@ -139,22 +116,6 @@ class TbFranqueador extends AbstractEntity
     public function setStNiveis($stNiveis)
     {
         $this->stNiveis = $stNiveis;
-    }
-
-    /**
-     * @return int
-     */
-    public function getIdConfiguracaoFranquia()
-    {
-        return $this->idConfiguracaoFranquia;
-    }
-
-    /**
-     * @param int $idConfiguracaoFranquia
-     */
-    public function setIdConfiguracaoFranquia($idConfiguracaoFranquia)
-    {
-        $this->idConfiguracaoFranquia = $idConfiguracaoFranquia;
     }
 
     /**
@@ -190,7 +151,7 @@ class TbFranqueador extends AbstractEntity
     }
 
     /**
-     * @return int
+     * @return string
      */
     public function getNoResponsavel()
     {
@@ -198,7 +159,7 @@ class TbFranqueador extends AbstractEntity
     }
 
     /**
-     * @param int $noResponsavel
+     * @param string $noResponsavel
      */
     public function setNoResponsavel($noResponsavel)
     {
@@ -206,7 +167,7 @@ class TbFranqueador extends AbstractEntity
     }
 
     /**
-     * @return int
+     * @return string
      */
     public function getNoEmail()
     {
@@ -214,11 +175,59 @@ class TbFranqueador extends AbstractEntity
     }
 
     /**
-     * @param int $noEmail
+     * @param string $noEmail
      */
     public function setNoEmail($noEmail)
     {
         $this->noEmail = $noEmail;
+    }
+
+    /**
+     * @return \TbConfiguracaoFranquia
+     */
+    public function getIdConfiguracaoFranquia()
+    {
+        return $this->idConfiguracaoFranquia;
+    }
+
+    /**
+     * @param \TbConfiguracaoFranquia $idConfiguracaoFranquia
+     */
+    public function setIdConfiguracaoFranquia($idConfiguracaoFranquia)
+    {
+        $this->idConfiguracaoFranquia = $idConfiguracaoFranquia;
+    }
+
+    /**
+     * @return \TbEndereco
+     */
+    public function getIdEndereco()
+    {
+        return $this->idEndereco;
+    }
+
+    /**
+     * @param \TbEndereco $idEndereco
+     */
+    public function setIdEndereco($idEndereco)
+    {
+        $this->idEndereco = $idEndereco;
+    }
+
+    /**
+     * @return \TbUsuario
+     */
+    public function getIdUsuario()
+    {
+        return $this->idUsuario;
+    }
+
+    /**
+     * @param \TbUsuario $idUsuario
+     */
+    public function setIdUsuario($idUsuario)
+    {
+        $this->idUsuario = $idUsuario;
     }
 }
 
