ac10ea0147bd3de0ecbaa20ab29cbb3b67251ef0
deniprogram <denissonfernades@hotmail.com>
2015-07-07 13:06:14 -0300

    FeedBack

---
+++ Codigo/src/Super/FeedbackBundle/Controller/FeedbackController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Super\FeedbackBundle\Controller;
+
+use Base\CrudBundle\Controller\CrudController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class FeedbackController  extends CrudController
+{
+    protected $serviceName = 'service.enquete';
+
+    public function emailAction()
+    {
+        return $this->render('SuperFeedbackBundle:Feedback:email.html.twig');
+    }
+
+}
---
+++ Codigo/src/Super/FeedbackBundle/SuperFeedbackBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Super\FeedbackBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SuperFeedbackBundle extends Bundle
+{
+}
