01d37e2c3d0618548857c87d9124dbdcd44b0d4f
deniprogram <denissonfernades@hotmail.com>
2015-07-08 18:32:27 -0300

    correcao de JS

--- Codigo/src/Super/PromocaoBundle/Service/Promocao.php
+++ Codigo/src/Super/PromocaoBundle/Service/Promocao.php
@@ -1,16 +1,12 @@
 <?php
-
 namespace Super\PromocaoBundle\Service;
-
 use Base\CrudBundle\Service\CrudService;
 use Base\BaseBundle\Entity\AbstractEntity;
 use Base\BaseBundle\Service\Data;
 use Symfony\Component\Validator\Validator;
-
 class Promocao extends CrudService
 {
     protected $entityName = 'Base\BaseBundle\Entity\TbPromocao';
-
     public function parserItens(array $itens = array(), $addOptions = false)
     {
         foreach ($itens as $key => $value) {
@@ -45,36 +41,27 @@ class Promocao extends CrudService
         }
         return parent::parserItens($itens, $addOptions);
     }
-
     public function preUpdate(AbstractEntity $entity = null)
     {
         $dtValidade = str_replace('/', '-', $this->getRequest()->request->get('dtValidade'));
         $this->entity->setDtValidade(Data::dateBr("$dtValidade 00:00"));
     }
-
-
     public function preSave(AbstractEntity $entity = null)
     {
         $this->entity->setDtCadastro(new \DateTime());
         $dtValidade = str_replace('/', '-', $this->getRequest()->request->get('dtValidade'));
         $this->entity->setDtValidade(Data::dateBr("$dtValidade 00:00"));
     }
-
-
     public function postSave(AbstractEntity $entity = null)
     {
         if ($this->getRequest()->files->get('noImagem')) {
             $path = $this->uploadFile('promocao/' . $this->entity->getIdPromocao(), 'noImagem');
-
             $this->entity->setNoImagem($path);
             $this->persist($this->entity);
         }
     }
-
-
     public function postRemove(AbstractEntity $entity = null)
     {
         $this->addMessage($this->container->get('translator')->trans('base_bundle.messages.success'));
     }
-
 }
