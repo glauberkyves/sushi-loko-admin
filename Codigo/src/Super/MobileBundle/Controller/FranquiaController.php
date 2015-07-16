671d77d23789dfde7bc5190c096c5ef11fadd473
Fellipe Vasconcelos <fellipe.fvs@gmail.com>
2015-07-16 18:53:22 -0300

    adiciona servicos mobile

--- Codigo/src/Super/MobileBundle/Controller/FranquiaController.php
+++ Codigo/src/Super/MobileBundle/Controller/FranquiaController.php
@@ -43,19 +43,75 @@ class FranquiaController extends AbstractMobile
     {
         $request = $this->getRequest();
 
-        $idFranquia = $this->getService('service.franqueador')->find($request->idFranquia);
+        $idFranquia = $this->getService('service.franquia')->find($request->idFranquia);
 
         if($idFranquia) {
 
+            $dtAtual = new \DateTime();
             $arrPromocao = array();
 
-            foreach($idFranquia->getIdFranquiaPromocao() as $key => $idPromocao) {
-                $arrPromocao[$key] = array(
-                    'noPromocao' => $idPromocao->getIdPromocao()->getNoPromocao(),
-                    'dsPromocao' => $idPromocao->getIdPromocao()->getDsPromocao()
-                );
+            foreach($idFranquia->getIdFranquiaPromocao() as $key => $idFranquiaPromocao) {
+                $idPromocao = $idFranquiaPromocao->getIdPromocao();
+                if($idPromocao->getStAtivo()) {
+                    if($idPromocao->getDtValidade()->format('u') >= $dtAtual->format('u')) {
+                        $arrPromocao[$key] = array(
+                            'noPromocao' => $idPromocao->getNoPromocao(),
+                            'dsPromocao' => $idPromocao->getDsPromocao()
+                        );
+                    }
+                }
             }
 
+            if($arrPromocao) {
+                $this->add('valido',    true);
+                $this->add('promocoes', $arrPromocao);
+            } else {
+                $this->add('mensagem', 'mobile_bundle.franquia.listar_promocao.error');
+            }
+        } else {
+            $this->add('mensagem', 'mobile_bundle.franquia.default.error');
+        }
+
+        return $this->response();
+    }
+
+    /**
+     * Listar cardapio de uma franquia
+     * @param idFranquia
+     * @return Response
+     */
+    public function listarCardapioAction()
+    {
+        $request = $this->getRequest();
+
+        $idFranquia = $this->getService('service.franquia')->find($request->idFranquia);
+
+        if($idFranquia) {
+
+            $arrProduto = array();
+            $idCardapio = $idFranquia->getIdCardapio();
+
+            foreach($idCardapio->getIdProduto() as $key => $idProduto) {
+                if($idProduto->getStAtivo()) {
+                    $imagem = $this->getService()->getInfoFile($idProduto->getNoImagem());
+                    $arrProduto[$key] = array(
+                        'noProduto' => $idProduto->getNoProduto(),
+                        'nuValor' => $idProduto->getNuValor(),
+                        'extensao' => $imagem['extensao'],
+                        'imagem' => $imagem['imagem']
+                    );
+                }
+            }
+
+            if($arrProduto) {
+                $this->add('valido',    true);
+                $this->add('cardapio', array(
+                    'noCardapio' => $idCardapio->getNoCardapio(),
+                    'produtos' => $arrProduto
+                ));
+            } else {
+                $this->add('mensagem', 'mobile_bundle.franquia.listar_cardapio.error');
+            }
         } else {
             $this->add('mensagem', 'mobile_bundle.franquia.default.error');
         }
