f6a02711c59941541b31121130b50284fc95dc32
Fellipe Vasconcelos <fellipe.fvs@gmail.com>
2015-08-31 18:32:11 -0300

    adiciona servico e repositorio de pesquisa

--- Codigo/src/Base/BaseBundle/Repository/PesquisaRepository.php
+++ Codigo/src/Base/BaseBundle/Repository/PesquisaRepository.php
@@ -12,20 +12,32 @@ use Doctrine\ORM\Query\Expr;
 use Doctrine\ORM\QueryBuilder;
 use Symfony\Component\HttpFoundation\Request;
 use Super\TransacaoBundle\Service\TipoTransacao;
+use Base\BaseBundle\Service\Pesquisa;
 
 class PesquisaRepository extends AbstractRepository
 {
     public function fetchGrid(Request $request)
     {
-        $subQueryCredito = $this->subQueryCredito($request);
-        $subQueryDebito  = $this->subQueryDebito($request);
+        $request = $this->padronizarRequest($request);
+
+        $subQueryCreditoTotal   = $this->subQueryCredito($request, false, 1);
+        $subQueryDebitoTotal    = $this->subQueryDebito($request, false, 2);
+        $subQueryCreditoPeriodo = $this->subQueryCredito($request, true, 3);
+        $subQueryDebitoPeriodo  = $this->subQueryDebito($request, true, 4);
+
+        $subQueryCount = $this->subQueryCountTransacao($request);
+        $subQueryMedia = $this->subQueryMediaConsumo($request);
 
         $query = $this
             ->getEntityManager()
             ->createQueryBuilder()
             ->select('u.idUsuario')
-            ->addSelect('(' . $subQueryCredito . ') credito')
-            ->addSelect('(' . $subQueryDebito . ') debito')
+            ->addSelect('(' . $subQueryCreditoTotal    . ') nuCreditoTotal')
+            ->addSelect('(' . $subQueryDebitoTotal     . ') nuDebitoTotal')
+            ->addSelect('(' . $subQueryCreditoPeriodo  . ') nuCreditoPeriodo')
+            ->addSelect('(' . $subQueryDebitoPeriodo   . ') nuDebitoPeriodo')
+            ->addSelect('(' . $subQueryCount           . ') nuTransacao')
+            ->addSelect('(' . $subQueryMedia           . ') nuMediaConsumo')
             ->from('Base\BaseBundle\Entity\TbUsuario', 'u')
             ->innerJoin('u.idPessoa', 'p')
             ->innerJoin('p.idPessoaFisica', 'pf');
@@ -35,113 +47,230 @@ class PesquisaRepository extends AbstractRepository
 
     public function addWhere(QueryBuilder $query, Request $request)
     {
-        $expr = new Expr();
-        foreach ($request->query->all() as $key => $value) {
+        $request = $request->query;
+
+        foreach ($request->all() as $key => $value) {
             switch($key) {
+                #filtro por sexo [u = unisex]
                 case 'sgSexo':
-                    if($value != 'u') {
-                        $query->andWhere("pf.$key = :sgSexo");
+                    if (strlen($value) == 1 && $value != 'u') {
+                        $query->andWhere("pf.{$key} = :sgSexo");
                         $query->setParameter('sgSexo', $value, \PDO::PARAM_STR);
                     }
                     break;
-                case 'nuSaldoBonus':
-                    if($value > 0) {
-                        $operador = $this->getOperador($request->query->get('noSaldoBonus'));
-                        $query->having("(credito - debito) $operador :saldo");
-                        $query->setParameter('saldo', $value, \PDO::PARAM_INT);
-                    }
-                    break;
+                #filtro por idade minima
                 case 'nuIdadeMin':
-                    if($value > 0) {
-                        $dtMin = new \DateTime();
-                        $dtMin->modify("-$value years");
-                        $query->andWhere("pf.dtNascimento <= :dtMin");
+                    if (is_numeric($value) && $value > 0) {
+                        $dtMin = $this->getDataModificada($value, 'a');
+                        $query->andWhere('pf.dtNascimento <= :dtMin');
                         $query->setParameter('dtMin', $dtMin);
                     }
                     break;
+                #filtro por idade maxima
                 case 'nuIdadeMax':
-                    if($value > 0) {
-                        $dtMax = new \DateTime();
-                        $dtMax->modify("-$value years");
-                        $query->andWhere("pf.dtNascimento >= :dtMax");
+                    if (is_numeric($value) && $value > 0) {
+                        $dtMax = $this->getDataModificada($value, 'a');
+                        $query->andWhere('pf.dtNascimento >= :dtMax');
                         $query->setParameter('dtMax', $dtMax);
+                    }
+                    break;
+                #filtro por atividade
+                case 'nuAtividade':
+                    if (is_numeric($value)) {
+                        switch (true) {
+                            case $value == 0: $query->having('nuTransacao >= 0'); break;
+                            case $value == 1: $query->having('nuTransacao > 0');  break;
+                            case $value == 2: $query->having('nuTransacao = 0');  break;
+                        }
+                    }
+                    break;
+                #filtro por quem obteve bonus
+                case 'nuCreditoPeriodo':
+                    if (is_numeric($value) && $value > 0) {
+                        $query->andHaving("nuCreditoPeriodo > 0");
+                    }
+                    break;
+                #filtro por quem utilizou bonus
+                case 'nuDebitoPeriodo':
+                    if (is_numeric($value) && $value > 0) {
+                        $query->andHaving("nuDebitoPeriodo > 0");
+                    }
+                    break;
+                #filtro por media de consumo
+                case 'nuMediaConsumo':
+                    $value = floatval(str_replace(',', '.', str_replace('.', '', $value)));
+                    if (is_numeric($value) && $value > 0) {
+                        $op = Pesquisa::getOperador($request->get('noMediaConsumo'));
+                        $query->andHaving("nuMediaConsumo {$op} :media");
+                        $query->setParameter('media', $value);
+                    }
+                    break;
+                #filtro por saldo de bonus
+                case 'nuSaldoBonus':
+                    $value = floatval(str_replace(',', '.', str_replace('.', '', $value)));
+                    if (is_numeric($value) && $value > 0) {
+                        $op = Pesquisa::getOperador($request->get('noSaldoBonus'));
+                        $query->andHaving("(nuCreditoTotal - nuDebitoTotal) {$op} :saldo");
+                        $query->setParameter('saldo', $value, \PDO::PARAM_INT);
+                    }
+                    break;
+                #filtro por período de cadastro
+                case 'nuCadastro':
+                    if (is_numeric($value) && $value > 0) {
+                        $dtCadastro = $this->getDataModificada($value, $request->get('noCadastro'));
+                        $query->andWhere('u.dtCadastro >= :dtCadastro');
+                        $query->setParameter('dtCadastro', $dtCadastro);
                     }
                     break;
             }
         }
     }
 
-    private function subQueryCredito($request)
+    private function subQueryCredito(Request $request, $filtrarPeriodo = false, $i = 1)
     {
-        $expr = new Expr();
+        $request = $request->query;
+        $expr    = new Expr();
 
         $query = $this
             ->getEntityManager()
             ->createQueryBuilder()
-            ->select('SUM(t1.nuValor)')
-            ->from('Base\BaseBundle\Entity\TbTransacao', 't1')
-            ->innerJoin('t1.idTipoTransacao', 'tt1')
-            ->innerJoin('t1.idUsuario', 'u1')
-            ->innerJoin('t1.idFranquia', 'ff1')
-            ->innerJoin('ff1.idFranqueador', 'f1')
-            ->where($expr->eq('u1.idUsuario', 'u.idUsuario'))
-            ->andWhere($expr->eq('t1.stAtivo', true))
-            ->andWhere($expr->eq('tt1.idTipoTransacao', TipoTransacao::CREDITO));
+            ->select("SUM(t{$i}.nuValor)")
+            ->from('Base\BaseBundle\Entity\TbTransacao', "t{$i}")
+            ->innerJoin("t{$i}.idTipoTransacao", "tt{$i}")
+            ->innerJoin("t{$i}.idUsuario", "u{$i}")
+            ->innerJoin("t{$i}.idFranquia", "ff{$i}")
+            ->innerJoin("ff{$i}.idFranqueador", "f{$i}")
+            ->where($expr->eq("f{$i}.idFranqueador", 56)) //alterar este valor
+            ->andWhere($expr->in("ff{$i}.idFranquia", $request->get("idFranqueado")))
+            ->andWhere($expr->eq("u{$i}.idUsuario", "u.idUsuario"))
+            ->andWhere($expr->eq("t{$i}.stAtivo", true))
+            ->andWhere($expr->eq("tt{$i}.idTipoTransacao", TipoTransacao::CREDITO));
 
-        if($idFranqueador = $request->query->get('idFranqueador')) {
-            $query->andWhere($expr->eq('ff1.idFranqueador', $idFranqueador));
+        if ($filtrarPeriodo) {
+            #filtro por pessoas que obtiveram bônus no periodo informado
+            $nuCreditoPeriodo = $request->get('nuCreditoPeriodo');
+            if (is_numeric($nuCreditoPeriodo) && $nuCreditoPeriodo > 0) {
+                $dtCadastro = $this->getDataModificada($nuCreditoPeriodo, $request->get('noCreditoPeriodo'));
+                $query->andWhere("t{$i}.dtCadastro >= '".$dtCadastro->format("Y-m-d H:i:s")."'");
+            }
         }
 
         return $query->getQuery()->getDQL();
     }
 
-    private function subQueryDebito($request)
+    private function subQueryDebito(Request $request, $filtrarPeriodo = false, $i = 2)
     {
-        $expr = new Expr();
+        $request = $request->query;
+        $expr    = new Expr();
 
         $query = $this
             ->getEntityManager()
             ->createQueryBuilder()
-            ->select('SUM(t2.nuValor)')
-            ->from('Base\BaseBundle\Entity\TbTransacao', 't2')
-            ->innerJoin('t2.idTipoTransacao', 'tt2')
-            ->innerJoin('t2.idUsuario', 'u2')
-            ->innerJoin('t2.idFranquia', 'ff2')
-            ->innerJoin('ff2.idFranqueador', 'f2')
-            ->where($expr->eq('u2.idUsuario', 'u.idUsuario'))
-            ->andWhere($expr->eq('t2.stAtivo', true))
-            ->andWhere($expr->eq('tt2.idTipoTransacao', TipoTransacao::DEBITO));
+            ->select("SUM(t{$i}.nuValor)")
+            ->from('Base\BaseBundle\Entity\TbTransacao', "t{$i}")
+            ->innerJoin("t{$i}.idTipoTransacao", "tt{$i}")
+            ->innerJoin("t{$i}.idUsuario", "u{$i}")
+            ->innerJoin("t{$i}.idFranquia", "ff{$i}")
+            ->innerJoin("ff{$i}.idFranqueador", "f{$i}")
+            ->where($expr->eq("f{$i}.idFranqueador", 56)) //alterar este valor
+            ->andWhere($expr->in("ff{$i}.idFranquia", $request->get("idFranqueado")))
+            ->andWhere($expr->eq("u{$i}.idUsuario", "u.idUsuario"))
+            ->andWhere($expr->eq("t{$i}.stAtivo", true))
+            ->andWhere($expr->eq("tt{$i}.idTipoTransacao", TipoTransacao::DEBITO));
 
-        if($idFranqueador = $request->query->get('idFranqueador')) {
-            $query->andWhere($expr->eq('ff2.idFranqueador', $idFranqueador));
+        if ($filtrarPeriodo) {
+            #filtro por pessoas que utilizaram bônus no periodo informado
+            $nuDebitoPeriodo = $request->get('nuDebitoPeriodo');
+            if (is_numeric($nuDebitoPeriodo) && $nuDebitoPeriodo > 0) {
+                $dtCadastro = $this->getDataModificada($nuDebitoPeriodo, $request->get('noDebitoPeriodo'));
+                $query->andWhere("t{$i}.dtCadastro >= '".$dtCadastro->format("Y-m-d H:i:s")."'");
+            }
         }
 
         return $query->getQuery()->getDQL();
     }
 
-    private function getOperador($operador = 'igual')
+    private function subQueryCountTransacao(Request $request)
     {
-        switch($operador)
-        {
-            case 'igual':
-                return '=';
-            case 'maior':
-                return '>';
-            case 'menor':
-                return '<';
+        $request = $request->query;
+        $expr    = new Expr();
+
+        $query = $this
+            ->getEntityManager()
+            ->createQueryBuilder()
+            ->select('COUNT(t5)')
+            ->from('Base\BaseBundle\Entity\TbTransacao', 't5')
+            ->innerJoin('t5.idTipoTransacao', 'tt5')
+            ->innerJoin('t5.idUsuario', 'u5')
+            ->innerJoin('t5.idFranquia', 'ff5')
+            ->innerJoin('ff5.idFranqueador', 'f5')
+            ->where($expr->eq('f5.idFranqueador', 56)) //alterar este valor
+            ->andWhere($expr->in('ff5.idFranquia', $request->get('idFranqueado')))
+            ->andWhere($expr->eq('u5.idUsuario', 'u.idUsuario'))
+            ->andWhere($expr->eq('t5.stAtivo', true))
+            ->andWhere($expr->in('tt5.idTipoTransacao', array(TipoTransacao::CREDITO, TipoTransacao::DEBITO)));
+
+        #filtro (nos ultimos dias ou meses)
+        $nuPeriodo = $request->get('nuPeriodo');
+        if (is_numeric($nuPeriodo) && $nuPeriodo > 0) {
+            $dtCadastro = $this->getDataModificada($nuPeriodo, $request->get('noCadastro'));
+            $query->andWhere("t5.dtCadastro >= '".$dtCadastro->format("Y-m-d H:i:s")."'");
         }
+
+        return $query->getQuery()->getDQL();
     }
 
-    private function getTempo($tempo = 'dia')
+    private function subQueryMediaConsumo(Request $request)
     {
-        switch($tempo)
+        $request = $request->query;
+        $expr    = new Expr();
+
+        $query = $this
+            ->getEntityManager()
+            ->createQueryBuilder()
+            ->select('AVG(t6.nuValor)')
+            ->from('Base\BaseBundle\Entity\TbTransacao', 't6')
+            ->innerJoin('t6.idTipoTransacao', 'tt6')
+            ->innerJoin('t6.idUsuario', 'u6')
+            ->innerJoin('t6.idFranquia', 'ff6')
+            ->innerJoin('ff6.idFranqueador', 'f6')
+            ->where($expr->eq('f6.idFranqueador', 56)) //alterar este valor
+            ->andWhere($expr->in('ff6.idFranquia', $request->get('idFranqueado')))
+            ->andWhere($expr->eq('u6.idUsuario', 'u.idUsuario'))
+            ->andWhere($expr->eq('t6.stAtivo', true))
+            ->andWhere($expr->eq('tt6.idTipoTransacao', TipoTransacao::DEBITO));
+
+        return $query->getQuery()->getDQL();
+    }
+
+    private function getDataModificada($nuPeriodo = 0, $noPeriodo = '')
+    {
+        if($nuPeriodo && $noPeriodo)
         {
-            case 'dia':
-                return 'days';
-            case 'mes':
-                return 'months';
-            case 'ano':
-                return 'years';
+            $noPeriodo  = Pesquisa::getPeriodo($noPeriodo);
+            $dateTime = new \DateTime();
+            $dateTime->modify("-{$nuPeriodo} {$noPeriodo}");
+
+            return $dateTime;
         }
+
+        return new \DateTime();
+    }
+
+    private function padronizarRequest(Request $request)
+    {
+        //caso seja uma busca por quem obteve e utilizou bonus
+        //sobrescrever as variaveis p/ continuar no fluxo normal
+        $r = $request->query;
+        if ($nuBonus = $r->get('nuBonusTransacionado')) {
+            if (is_numeric($nuBonus) && $nuBonus > 0) {
+                $request->query->set('nuDebitoPeriodo',  $nuBonus);
+                $request->query->set('nuCreditoPeriodo', $nuBonus);
+                $request->query->set('noDebitoPeriodo',  $r->get('noBonusTransacionado'));
+                $request->query->set('noCreditoPeriodo', $r->get('noBonusTransacionado'));
+            }
+        }
+
+        return $request;
     }
 }
